7f1744f0d7a612f58548ab99c6b507202de20a80
Khilmi Wahyu Saputra <khilmiwahyu27@gmail.com>
2026-05-11 15:10:58 +0700

    Menambahkan background partikel

--- uts_frontend/js/script.js
+++ uts_frontend/js/script.js
@@ -21,10 +21,11 @@ const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
             navbar.classList.add('scrolled');
-            } else {
-                navbar.classList.remove('scrolled');
-            }
-    });
+    }
+    else {
+        navbar.classList.remove('scrolled');
+    }
+});
 
 
 // Faq
